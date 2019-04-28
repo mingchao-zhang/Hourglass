1e62280261e3e61408ae434ade6c8c16e4aaffbd
kys2 <44933652+kys2@users.noreply.github.com>
2019-04-28 01:29:49 -0500

    Add description to manifest.

--- public/background.js
+++ public/background.js
@@ -1,1 +1,18 @@
-console.log(new Date());
+setInterval(() => {
+	chrome.idle.queryState(60, (newState) => {
+		console.log(newState);
+		if (newState !== "locked") {
+			chrome.windows.getLastFocused((window) => {
+				console.log("window", window.id, window.focused);
+				if (window.focused) {
+					chrome.tabs.query({active: true, lastFocusedWindow: true}, (result) => {
+						console.log("tab", result[0].id, result[0].url);
+						let url = new URL(result[0].url);
+						console.log(url.hostname.replace(/^w+\d*\./, ""));
+					});
+				}
+			});
+		}
+	});
+	console.log("");
+}, 1000);
