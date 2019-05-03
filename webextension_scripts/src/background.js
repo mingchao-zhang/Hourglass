13ed4fd68a618090679f1aaf99cd885e2b05b4c2
Kevin <kys2@illinois.edu>
2019-05-03 02:04:05 -0500

    Listener callback base completed.

--- webextension_scripts/src/background.js
+++ webextension_scripts/src/background.js
@@ -2,7 +2,7 @@ import browser from "webextension-polyfill";
 import dexie from "dexie";
 
 /*
-	User browsing history is stored in a Timeline, a collection of Events.
+	User browsing history is saved in a Timeline, a collection of Events.
 
 	An Event is a period of time spent at a specific site. Sites are identified by their URL's hostname.
 	i.e. "https://site.jp", "https://site.jp/<path>", and "http://site.jp" all evaluate to "site.jp".
@@ -22,48 +22,211 @@ import dexie from "dexie";
 	Initialization on startup.
 */
 
-// Create a buffer which holds the Event to insert into the Timeline.
+// List of desired URL schemes to match, including the final ':'.
+var schemes = [
+	"http:",
+	"https:"
+];
+
+// Buffer which holds the Event to insert into the Timeline.
 var event_buffer = {
 	url_hostname: undefined,
 	begin: undefined,
 	end: undefined
 };
 
+/*
+	Write the Event buffer to the Timeline.
+
+	Parameters:
+	date
+		The date/time when the calling listener activated.
+	url (optional)
+		The new site. If unspecified, the Event buffer's site is set to undefined.
+
+	1.	Reduce date/time precision to 1 minute.
+	2.	If the Event buffer's site is NOT undefined:
+		a.	Write the Event buffer to the Timeline.
+		b.	Set the Event buffer's site to undefined.
+	3.	If the new site's scheme matches:
+		a.	Set the Event buffer's site to the new site and times to the saved date/time.
+		b.	Write the Event buffer to the Timeline.
+		c.	Create the autosave alarm.
+	4.	Otherwise:
+		a.	Clear the autosave alarm.
+		b.	Set the Event buffer's site to undefined.
+*/
+
+function Write_Event_Buffer(date, url = {}) {
+	// Reduce date/time precision to 1 minute.
+	date.setSeconds(0);
+	date.setMilliseconds(0);
+	// If the Event buffer's site is NOT undefined:
+	if (event_buffer.url_hostname !== undefined) {
+		// Write the Event buffer to the Timeline.
+		event_buffer.end = date;
+
+	}
+	// If the new site's scheme matches:
+	if (schemes.includes(url.protocol)) {
+		// Set the Event buffer's site to the new site and times to the saved date/time.
+		event_buffer.url_hostname = url.hostname;
+		event_buffer.begin = date;
+		event_buffer.end = date;
+		// Write the Event buffer to the Timeline.
+
+		// Create the autosave alarm.
+		browser.alarms.create("autosave", {periodInMinutes: 1});
+	}
+	// Otherwise:
+	else {
+		// Clear the autosave alarm.
+		browser.alarms.clear("autosave");
+		// Set the Event buffer's site to undefined.
+		event_buffer.url_hostname = undefined;
+	}
+}
+
+/*
+	Alarms:
+	autosave
+		Autosave the user's browsing history.
+*/
+
+browser.alarms.onAlarm.addListener(alarmInfo => {
+	if (alarmInfo.name === "autosave") {
+		/*
+			Autosave the user's browsing history. Assume the browser is in focus and the URL scheme matches.
+
+			1.	Save the current date/time.
+			2.	Find the active tab in the last focused window.
+			3.	Call the Write_Event_Buffer function.
+		*/
+
+		// Save the current date/time.
+		let date = new Date();
+		// Find the active tab in the last focused window.
+		browser.tabs.query({active: true, lastFocusedWindow: true})
+			.then(tabs => {
+				let url = new URL(tabs[0].url);
+				// Call the Write_Event_Buffer function.
+				Write_Event_Buffer(date, url);
+
+				// Debug.
+				browser.notifications.create("Autosave", {
+					type: "basic",
+					iconUrl: "icon.png",
+					title: "Autosave.",
+					message: event_buffer.url_hostname || "undefined"
+				});
+			});
+	}
+});
+
 // Open the database.
 
 
 /*
+	When the system's state changes.
+	1.	Save the current date/time.
+	2.	Find the last focused window and check if it's in focus (i.e. if the browser is in focus). Populate the Window object with its tabs.
+	3.	If the system is locked:
+		a.	Call the Write_Event_Buffer function sans url.
+	4.	If the system is idle or active:
+		a.	Check if the URL hostname (i.e. the site) changed.
+		b.	Call the Write_Event_Buffer function.
+*/
+
+browser.idle.onStateChanged.addListener((newState) => {
+	// Save the current date/time.
+	let date = new Date();
+	// Find the last focused window and check if it's in focus (i.e. if the browser is in focus). Populate the Window object with its tabs.
+	browser.windows.getLastFocused({populate: true})
+		.then(windowInfo => {
+			if (windowInfo.focused) {
+				// If the system is locked:
+				if (newState === "locked") {
+
+					// Debug.
+					if (event_buffer.url_hostname !== undefined) {
+						browser.notifications.create("Site changed from system state change", {
+							type: "basic",
+							iconUrl: "icon.png",
+							title: "Site changed from system changing from idle/active to locked.",
+							message: event_buffer.url_hostname || "undefined"
+						});
+					}
+
+					// Call the Write_Event_Buffer function sans url.
+					Write_Event_Buffer(date);
+				}
+				// If the system is idle or active:
+				else {
+					// Check if the URL hostname (i.e. the site) changed.
+					let url = new URL(windowInfo.tabs.filter(tab => tab.active)[0].url);
+					if (url.hostname !== event_buffer.url_hostname) {
+						// Call the Write_Event_Buffer function.
+						Write_Event_Buffer(date, url);
+
+						// Debug.
+						browser.notifications.create("Site changed from system state change", {
+							type: "basic",
+							iconUrl: "icon.png",
+							title: "Site changed from system changing from locked to active, idle to active, or active to idle.",
+							message: event_buffer.url_hostname || "undefined"
+						});
+					}
+				}
+			}
+		});
+});
+
+/*
 	When a tab in any window is updated:
-	1.	Store the current date/time.
+	1.	Save the current date/time.
 	2.	Check if the update is a URL change (i.e. a potential site change).
-	3.	Find the last focused window and check if it's in focus (i.e. if the browser is in focus). Populate the Window object with its tabs.
-	4.	Check if the updated tab is the last focused window's active tab.
-	5.	Check if the URL hostname (i.e. the site) changed.
+	3.	Check if the system is unlocked.
+	4.	Find the last focused window and check if it's in focus (i.e. if the browser is in focus). Populate the Window object with its tabs.
+	5.	Check if the updated tab is the last focused window's active tab.
+	6.	Check if the URL hostname (i.e. the site) changed.
+	7.	Call the Write_Event_Buffer function.
 */
 
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
-	// Store the current date/time.
+	// Save the current date/time.
 	let date = new Date();
-	// Check if the update is a URL change.
+	// Check if the update is a URL change (i.e. a potential site change).
 	if (changeInfo.url) {
-		// Find the last focused window and check if it's in focus (i.e. if the browser is in focus). Populate the Window object with its tabs.
-		browser.windows.getLastFocused({populate: true})
-			.then(windowInfo => {
-				if (windowInfo.focused) {
-					// Check if the updated tab is the last focused window's active tab.
-					if (tabId === windowInfo.tabs.filter(tab => tab.active)[0].id) {
-						// Check if the URL hostname (i.e. the site) changed.
-						let url_hostname = new URL(changeInfo.url).hostname;
-						if (url_hostname !== event_buffer.url_hostname) {
-							event_buffer.url_hostname = url_hostname;
-							browser.notifications.create("Site changed in active tab", {
-								type: "basic",
-								iconUrl: "icon.png",
-								title: "Site changed in active tab.",
-								message: event_buffer.url_hostname
-							});
-						}
-					}
+		// Debug.
+		console.log(changeInfo.url);
+
+		// Check if the system is unlocked.
+		browser.idle.queryState(60)
+			.then(newState => {
+				if (newState !== "locked") {
+					// Find the last focused window and check if it's in focus (i.e. if the browser is in focus). Populate the Window object with its tabs.
+					browser.windows.getLastFocused({populate: true})
+						.then(windowInfo => {
+							if (windowInfo.focused) {
+								// Check if the updated tab is the last focused window's active tab.
+								if (tabId === windowInfo.tabs.filter(tab => tab.active)[0].id) {
+									// Check if the URL hostname (i.e. the site) changed.
+									let url = new URL(changeInfo.url);
+									if (url.hostname !== event_buffer.url_hostname) {
+										// Call the Write_Event_Buffer function.
+										Write_Event_Buffer(date, url);
+
+										// Debug.
+										browser.notifications.create("Site changed in active tab", {
+											type: "basic",
+											iconUrl: "icon.png",
+											title: "Site changed in active tab.",
+											message: event_buffer.url_hostname || "undefined"
+										});
+									}
+								}
+							}
+						});
 				}
 			});
 	}
@@ -71,191 +234,101 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
 
 /*
 	When the active tab in any window is changed:
-	1.	Store the current date/time.
-	2.	Find the last focused window and check if it's in focus (i.e. if the browser is in focus). Populate the Window object with its tabs.
-	3.	Check if the activated tab is in the last focused window.
-	4.	Check if the URL hostname (i.e. the site) changed.
+	1.	Save the current date/time.
+	2.	Check if the system is unlocked.
+	3.	Find the last focused window and check if it's in focus (i.e. if the browser is in focus). Populate the Window object with its tabs.
+	4.	Check if the activated tab is in the last focused window.
+	5.	Check if the URL hostname (i.e. the site) changed.
+	6.	Call the Write_Event_Buffer function.
 */
 
 browser.tabs.onActivated.addListener(activeInfo => {
-	// Store the current date/time.
+	// Save the current date/time.
 	let date = new Date();
-	// Find the last focused window and check if it's in focus (i.e. if the browser is in focus). Populate the Window object with its tabs.
-	browser.windows.getLastFocused({populate: true})
-		.then(windowInfo => {
-			if (windowInfo.focused) {
-				// Check if the activated tab is in the last focused window.
-				if (activeInfo.windowId === windowInfo.id) {
-					// Check if the URL hostname (i.e. the site) changed.
-					let url_hostname = new URL(windowInfo.tabs.filter(tab => tab.id === activeInfo.tabId)[0].url).hostname;
-					if (url_hostname !== event_buffer.url_hostname) {
-						event_buffer.url_hostname = url_hostname;
-						browser.notifications.create("Site changed from tab switch", {
-							type: "basic",
-							iconUrl: "icon.png",
-							title: "Site changed from tab switch.",
-							message: event_buffer.url_hostname
-						});
-					}
-				}
+	// Check if the system is unlocked.
+	browser.idle.queryState(60)
+		.then(newState => {
+			if (newState !== "locked") {
+				// Find the last focused window and check if it's in focus (i.e. if the browser is in focus). Populate the Window object with its tabs.
+				browser.windows.getLastFocused({populate: true})
+					.then(windowInfo => {
+						if (windowInfo.focused) {
+							// Check if the activated tab is in the last focused window.
+							if (activeInfo.windowId === windowInfo.id) {
+								// Check if the URL hostname (i.e. the site) changed.
+								let url = new URL(windowInfo.tabs.filter(tab => tab.id === activeInfo.tabId)[0].url);
+								if (url.hostname !== event_buffer.url_hostname) {
+									// Call the Write_Event_Buffer function.
+									Write_Event_Buffer(date, url);
+
+									// Debug.
+									browser.notifications.create("Site changed from tab switch", {
+										type: "basic",
+										iconUrl: "icon.png",
+										title: "Site changed from tab switch.",
+										message: event_buffer.url_hostname || "undefined"
+									});
+								}
+							}
+						}
+					});
 			}
 		});
 });
 
 /*
 	When the currently focused window changes:
-	1.	Store the current date/time.
-	2.	If there is no browser window:
-		a.	Insert an Event into the Timeline, then set its site fo undefined.
-	3.	If there is a browser window:
+	1.	Save the current date/time.
+	2.	Check if the system is unlocked.
+	3.	If there is no browser window:
+		a.	Call the Write_Event_Buffer function sans url.
+	4.	If there is a browser window:
 		a.	Find the window. Populate the Window object with its tabs.
-	3.	Check if the URL hostname (i.e. the site) changed.
+		b.	Check if the URL hostname (i.e. the site) changed.
+		c.	Call the Write_Event_Buffer function.
 */
 
 browser.windows.onFocusChanged.addListener(windowId => {
-	// Store the current date/time.
+	// Save the current date/time.
 	let date = new Date();
-	// If there is no browser window:
-	if (windowId === browser.windows.WINDOW_ID_NONE) {
-		event_buffer.url_hostname = undefined;
-		browser.notifications.create("Site changed from browser defocus", {
-			type: "basic",
-			iconUrl: "icon.png",
-			title: "Site changed from browser defocus or devtools focus.",
-			message: ""
-		});
-	}
-	// If there is a browser window:
-	else {
-		// Find the window.
-		browser.windows.get(windowId, {populate: true})
-			.then(windowInfo => {
-				// Check if the URL hostname (i.e. the site) changed.
-				let url_hostname = new URL(windowInfo.tabs.filter(tab => tab.active)[0].url).hostname;
-				if (url_hostname !== event_buffer.url_hostname) {
-					event_buffer.url_hostname = url_hostname;
-					browser.notifications.create("Site changed from browser focus", {
+	// Check if the system is unlocked.
+	browser.idle.queryState(60)
+		.then(newState => {
+			if (newState !== "locked") {
+				// If there is no browser window:
+				if (windowId === browser.windows.WINDOW_ID_NONE) {
+					// Call the Write_Event_Buffer function sans url.
+					Write_Event_Buffer(date);
+
+					// Debug.
+					browser.notifications.create("Site changed from browser defocus", {
 						type: "basic",
 						iconUrl: "icon.png",
-						title: "Site changed from browser focus.",
-						message: event_buffer.url_hostname
+						title: "Site changed from browser defocus or devtools focus.",
+						message: event_buffer.url_hostname || "undefined"
 					});
 				}
-			});
-	}
-});
-
-/*
-	When the system's state changes.
-	1.	Store the current date/time.
-	2.	Find the last focused window and check if it's in focus (i.e. if the browser is in focus). Populate the Window object with its tabs.
-	3.	If the system is locked:
-		a.	Insert the Event into the Timeline, then set its site to undefined.
-	4.	If the system is idle or active:
-		a.	Check if the URL hostname (i.e. the site) changed.
-*/
-
-browser.idle.onStateChanged.addListener((newState) => {
-	// Store the current date/time.
-	let date = new Date();
-	// Find the last focused window and check if it's in focus (i.e. if the browser is in focus). Populate the Window object with its tabs.
-	browser.windows.getLastFocused({populate: true})
-		.then(windowInfo => {
-			if (windowInfo.focused) {
-				// If the system is locked, check if the Event's site isn't undefined and insert it.
-				if (newState === "locked") {
-					if (event_buffer.url_hostname !== undefined) {
-						event_buffer.url_hostname = undefined;
-						browser.notifications.create("Site changed from system state change", {
-							type: "basic",
-							iconUrl: "icon.png",
-							title: "Site changed from system changing from idle or active to locked.",
-							message: event_buffer.url_hostname
-						});
-					}
-				}
+				// If there is a browser window:
 				else {
-					let url_hostname = new URL(windowInfo.tabs.filter(tab => tab.active)[0].url).hostname;
-					if (url_hostname !== event_buffer.url_hostname) {
-						event_buffer.url_hostname = url_hostname;
-						browser.notifications.create("Site changed from system state change", {
-							type: "basic",
-							iconUrl: "icon.png",
-							title: "Site changed from system changing locked to idle or active (probably).",
-							message: event_buffer.url_hostname
+					// Find the window. Populate the Window object with its tabs.
+					browser.windows.get(windowId, {populate: true})
+						.then(windowInfo => {
+							// Check if the URL hostname (i.e. the site) changed.
+							let url = new URL(windowInfo.tabs.filter(tab => tab.active)[0].url);
+							if (url.hostname !== event_buffer.url_hostname) {
+								// Call the Write_Event_Buffer function.
+								Write_Event_Buffer(date, url);
+
+								// Debug.
+								browser.notifications.create("Site changed from browser focus", {
+									type: "basic",
+									iconUrl: "icon.png",
+									title: "Site changed from browser focus.",
+									message: event_buffer.url_hostname || "undefined"
+								});
+							}
 						});
-					}
 				}
 			}
 		});
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Testing.
-/*
-// Triggers when the active tab in ANY window is changed.
-/*
-browser.windows.onFocusChanged.addListener((windowId) => {
-	if (windowId === browser.windows.WINDOW_ID_NONE) {
-		browser.notifications.create("window_switch", {type: "basic", iconUrl: "icon.png", title: "Window Switch", message: "All browser windows have lost focus."});
-		console.log("All browser windows have lost focus.");
-	}
-	else {
-		browser.notifications.create("window_switch", {type: "basic", iconUrl: "icon.png", title: "Window Switch", message: "Window focus changed to " + windowId.toString() + "."});
-		console.log("Window focus changed to " + windowId.toString() + ".");
-	}
-});
-*/
-/*
-setInterval(() => {
-	browser.idle.queryState(60, (newState) => {
-		console.log(newState);
-		if (newState !== "locked") {
-			browser.windows.getLastFocused((window) => {
-				console.log("window", window.id, window.focused);
-				if (window.focused) {
-					browser.tabs.query({active: true, lastFocusedWindow: true}, (result) => {
-						console.log("tab", result[0].id, result[0].url);
-						let url = new URL(result[0].url);
-						console.log(url.hostname.replace(/^w+\d*\./, ""));
-					});
-				}
-			});
-		}
-	});
-	console.log("");
-}, 1000);
-*/
